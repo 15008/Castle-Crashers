ce72d466c314d9f3fae0a54c61d5e03f2cfa90f6
15008 <42592231+15008@users.noreply.github.com>
2019-06-03 18:56:38 +1200

    Corrections and adjustments
    Testing pages to see if names load, correction with db and models. New addition with pages and routes.

--- castle_crashers/app/models.py
+++ castle_crashers/app/models.py
@@ -1,43 +1,50 @@
 from app import db
 
-class Animals(db.Model):
-  __tablename__ = 'animalOrbs'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(100))
-  location = db.Column(db.String(10))
-  cost = db.Column(db.Integer)
-  description = db.Column(db.String(2000))
-  requirement = db.Column(db.String(1000))
-  magicBonus = db.Column(db.Integer)
-  strengthBonus = db.Column(db.Integer)
-  defenseBonus = db.Column(db.Integer)
-  agilityBonus = db.Column(db.Integer)
-  xpMultiplier = db.Column(db.Integer)
+class Orb(db.Model):
+    __tablename__ = 'Orb'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    locationid = db.Column(db.Integer, db.ForeignKey('Location.id'), nullable=False)
+    description = db.Column(db.String(2000))
+    cost = db.Column(db.Integer)
+    req = db.Column(db.String(1000))
+    magicBonus = db.Column(db.Integer)
+    strengthBonus = db.Column(db.Integer)
+    defenseBonus = db.Column(db.Integer)
+    agilityBonus = db.Column(db.Integer)
+    xpBonus = db.Column(db.Integer)
   #roles = db.relationship('Role', backref = 'movies')
   #def __repr__(self):
     #return 'Movie: {}'.format(self.title)
 
+class Location(db.Model):
+    __tablename__ = 'Location'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(2000))
+    chapter = db.Column(db.Integer, nullable=False)
+    localeType = db.Column(db.String(10))
+  #movies = db.relationship('Movie', secondary = 'name', backref = 'actors')
+  #def __repr__(self):
+    #return 'Name: {}'.format(self.name)
 
-
-class Actor(db.Model):
-  __tablename__ = 'Actor'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(400))
-  dob = db.Column(db.String(10))
-  description = db.Column(db.String(2000))
-  movies = db.relationship('Movie', secondary = 'name', backref = 'actors')
-  def __repr__(self):
-    return 'Name: {}'.format(self.name)
-
-
-class Role(db.Model):
-  __tablename__ = 'Role'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(400))
-  superName = db.Column(db.String(400))
-  mid = db.Column(db.Integer, db.ForeignKey('movies.id'))
-  aid = db.Column(db.Integer, db.ForeignKey('Actor.id'))
-
-
-  def __repr__(self):
-    return 'Role: {}'.format(self.role)
+class Weapon(db.Model):
+    __tablename__ = 'Weapon'
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    locationid = db.Column(db.Integer, db.ForeignKey('Location.id'), nullable=False)
+    reqItem = db.Column(db.String(2000))
+    reqLevel = db.Column(db.Integer, nullable=False)
+    mode = db.Column(db.String(10), nullable=False)
+    strength = db.Column(db.Integer)
+    magic = db.Column(db.Integer)
+    defense = db.Column(db.Integer)
+    agility = db.Column(db.Integer)
+    iceSpecial = db.Column(db.Integer)
+    critSpecial = db.Column(db.Integer)
+    fireSpecial = db.Column(db.Integer)
+    elecSpecial = db.Column(db.Integer)
+    poiSpecial = db.Column(db.Integer)
+    cost = db.Column(db.Integer)
+  #def __repr__(self):
+    #return 'Role: {}'.format(self.role)
--- castle_crashers/app/routes.py
+++ castle_crashers/app/routes.py
@@ -1,19 +1,39 @@
 from flask import render_template, redirect, url_for
 from app import app, db
-from app.models import Movie
-from app.models import Actor
-
+from app.models import Orb
+from app.models import Location
+from app.models import Weapon
 
 @app.route('/')
 def home():
     return render_template('home.html', page_title="Home Page")
 
-@app.route('/all_movies')
-def all_movies():
-	movies = Movie.query.all()
-	return render_template('all_movies.html',page_title="Movie Names", movies=movies)
+@app.route('/all_orbs')
+def all_orbs():
+	orbs = Orb.query.all()
+	return render_template('all_orbs.html',page_title="Animal Orbs", orbs=orbs)
 
-@app.route("/movie/<id>")
-def movie(id):
-    movie = Movie.query.filter_by(id = id).first()
-    return render_template('movie_details.html', page_title="Details", movie=movie)
+@app.route("/orb/<id>")
+def orb_details(id):
+    orb = Orb.query.filter_by(id = id).first()
+    return render_template('orb_details.html', page_title="Animal Orb Details", orb=orb)
+
+@app.route('/all_locations')
+def all_locations():
+	locations = Location.query.all()
+	return render_template('all_locations.html',page_title="Locations", locations=locations)
+
+@app.route("/location/<id>")
+def location_details(id):
+    location = Location.query.filter_by(id = id).first()
+    return render_template('location_details.html', page_title="Location Details", location=location)
+
+@app.route('/all_weapons')
+def all_weapons():
+	weapons = Weapon.query.all()
+	return render_template('all_weapons.html',page_title="Weapons", weapons=weapons)
+
+@app.route("/weapon/<id>")
+def weapon_details(id):
+    weapon = Weapon.query.filter_by(id = id).first()
+    return render_template('weapon_details.html', page_title="Weapon Details", weapon=weapon)
