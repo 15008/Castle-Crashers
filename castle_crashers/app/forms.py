28ff78376a6d8c9c81f28d71d6cdc015b035bd49
15008 <42592231+15008@users.noreply.github.com>
2019-07-24 09:58:46 +1200

    Mid Point Backup
    Additions with CSS and form changes.

--- castle_crashers/app/forms.py
+++ castle_crashers/app/forms.py
@@ -4,16 +4,35 @@ from wtforms.validators import DataRequired, Optional, ValidationError
 import app.models
 from datetime import datetime
 
-class Add_Location(FlaskForm):
-    submitAL = SubmitField('submit')
+class Edit_Location(FlaskForm):
     name = TextField('name', validators=[DataRequired()])
     description = TextAreaField('description')
     chapter = IntegerField('chapter', validators=[DataRequired()])
     localeType = TextField('localeType')
 
-class Remove_Location(FlaskForm):
-    submitRL = SubmitField('submit')
+class Edit_Weapon(FlaskForm):
     name = TextField('name', validators=[DataRequired()])
+    reqItem = TextAreaField('reqItem')
+    reqLevel = IntegerField('reqLevel', validators=[DataRequired()])
+    mode = TextField('mode', validators=[DataRequired()])
+    strength = IntegerField('strength')
+    magic = IntegerField('magic')
+    defense = IntegerField('defense')
+    agility = IntegerField('agility')
+    iceSpecial = IntegerField('iceSpecial')
+    critSpecial = IntegerField('critSpecial')
+    fireSpecial = IntegerField('fireSpecial')
+    elecSpecial = IntegerField('elecSpecial')
+    poiSpecial = IntegerField('poiSpecial')
+    cost = IntegerField('cost')
 
-#class Select_Location(FlaskForm):
-#    localeType = SelectField('localeType', validator=[DataRequired()])
+class Edit_Orb(FlaskForm):
+    name = TextField('name', validators=[DataRequired()])
+    description = TextAreaField('description')
+    cost = IntegerField('cost')
+    req = TextAreaField('req')
+    magicBonus = IntegerField('magicBonus')
+    strengthBonus = IntegerField('strengthBonus')
+    defenseBonus = IntegerField('defenseBonus')
+    agilityBonus = IntegerField('agilityBonus')
+    xpBonus = IntegerField('xpBonus')
--- castle_crashers/app/routes.py
+++ castle_crashers/app/routes.py
@@ -1,9 +1,7 @@
 from flask import render_template, redirect, url_for, request
 from app import app, db
-from app.models import Orb
-from app.models import Location
-from app.models import Weapon
-from app.forms import Add_Location
+from app.models import Orb, Location, Weapon
+from app.forms import Add_Location, Edit_Orb
 
 @app.route('/')
 def home():
@@ -25,10 +23,16 @@ def all_locations():
 	locations = Location.query.all()
 	return render_template('all_locations.html',page_title="Locations", locations=locations)
 
-@app.route('/location/<id>')
+@app.route('/location/<id>', methods=['GET', 'POST'])
 def location_details(id):
     location = Location.query.filter_by(id = id).first()
-    return render_template('location_details.html', page_title="Location Details", location=location)
+    form = Remove_Location()
+    if form.validate_on_sumbit():
+        db.session.delete(location)
+        db.session.commit()
+        return render_template('home.html', page_title="Home Page")
+    else:
+        return render_template('location_details.html', page_title="Location Details", location=location, form=form)
 
 @app.route('/all_weapons')
 def all_weapons():
@@ -41,37 +45,85 @@ def weapon_details(id):
     location = Location.query.filter_by(id = id).first()
     return render_template('weapon_details.html', page_title="Weapon Details", weapon=weapon, location=location)
 
-@app.route('/contact', methods=['GET', 'POST'])
+@app.route('/contact')
 def contact():
-    formAL = Add_Location()
-    formRL = Remove_location()
-    if request.method=='GET':
-        return render_template('contact.html', formAL=formAL, title="Contact")
-    else:
-        if formAL.submitAL.data and formAL.validate():
-            new_location = Location()
-            new_location.name = formAL.name.data
-            new_location.description = formAL.description.data
-            new_location.chapter = formAL.chapter.data
-            new_location.localeType = formAL.localeType.data
-            db.session.add(new_location)
-            db.session.commit()
-            return redirect(url_for('location_details', id=new_location.id))
-        else:
-            return render_template('contact.html', formAL=formAL, title="Contact")
-    if request.method=='GET':
-        return render_template('contact.html', formRL=formRL, title="Contact")
-    else:
-        if formRL.submitRL.data and formRL.validate():
-            want_locale = Location()
-            want_locale.name = formRL.name.data
-            db.session.remove(want_locale)
-            db.session.commit()
-            return redirect(url_for('success'))
-        else:
-            return render_template('contact.html', formRL=formRL, title="Contact")
+    return render_template('contact.html', title="Contact")
 
-#Error Handler
-#@app.errorhandler(404)
-#    def page_not_found(e):
-#        return render_template('404.html', error=str(e))
+@app.route('/edit_location', methods=['GET', 'POST'])
+def add_location():
+    form = Add_Location()
+    if form.validate_on_submit():
+        new_location = Location()
+        new_location.name = form.name.data
+        new_location.description = form.description.data
+        new_location.chapter = form.chapter.data
+        new_location.localeType = form.localeType.data
+        db.session.add(new_location)
+        db.session.commit()
+        return redirect(url_for('location_details.html', id=new_location.id))
+    else:
+        return render_template('edit_location.html', form=form, title="Add Location")
+
+#@app.route('/add_weapon', method=['GET', 'POST'])
+#def add_weapon():
+#    form = Add_Weapon()
+#    if form.validate_on_submit():
+#        new_weapon = Weapon()
+#        new_weapon.name = form.name.data
+#        new_weapon.reqItem = form.reqItem.data
+#        new_weapon.reqLevel = form.reqLevel.data
+#        new_weapon.mode = form.mode.data
+#        new_weapon.strength = form.strength.data
+#        new_weapon.magic = form.magic.data
+#        new_weapon.defense = form.defense.data
+#        new_weapon.agility = form.agility.data
+#        new_weapon.iceSpecial = form.iceSpecial.data
+#        new_weapon.critSpecial = form.critSpecial.data
+#        new_weapon.fireSpecial = form.fireSpecial.data
+#        new_weapon.elecSpecial = form.elecSpecial.data
+#        new_weapon.poiSpecial = form.poiSpecial.data
+#        new_weapon.cost = form.cost.data
+#        db.session.add(new_weapon)
+#        db.session.commit()
+#        return redirect(url_for('weapon_detail.html', id=new_weapon.id))
+#    else:
+#        return render_template('remove_weapon.html', form=form, title="Remove Weapon")
+
+#@app.route('/remove_weapon', methods=['GET', 'POST'])
+#def remove_weapon():
+#    form = Remove_Weapon()
+#    if form.validate_on_submit():
+#        ex_weapon = Weapon.query.filter_by(name = form.name.data).first()
+#        db.session.delete(ex_weapon)
+#        db.session.commit()
+#        return render_template('success.html', page_title="Success")
+#    else:
+#        return render_template('remove_weapon.html', form=form, title="Remove Weapon")
+
+#@app.route('/edit_orb', method=['GET', 'POST'])
+#def edit_orb():
+#    form = Edit_Orb()
+#    if form.validate_on_submit():
+#        new_orb.description = form.description.data
+#        new_orb.cost = form.cost.data
+#        new_orb.req = form.req.data
+#        new_orb.magicBonus = form.magicBonus.data
+#        new_orb.strengthBonus = form.strength.data
+#        new_orb.defenseBonus = form.defense.data
+#        new_orb.agilityBonus = form.agilityBonus.data
+#        new_orb.xpBonus = form.xpBonus.data
+#        if add_data:
+#            new_orb.name = form.name.data
+#            db.session.add(new_orb)
+#            db.session.commit()
+#            return redirect(url_for('orb_detail.html', id=new_orb.id))
+#        elif remove_data:
+#            ex_orb = Orb.query.filter_by(name = form.name.data).first()
+#            db.session.delete(ex_orb)
+#            db.session.commit()
+#            return render_template('success.html', page_title="Success")
+#        elif update_data:
+#            ex_orb.name = Orb.query.filter_by(name = form.name.data).first()
+#            db.session.add(ex_orb)
+#    else:
+#        return render_template('edit_orb.html', form=form, title = "Edit Orbs")
